23fb87e05852828eb5e6a8f05102573d196baeaf
MrFedko <miklerst@mail.ru>
2025-08-25 00:02:09 +0300

    remove fastapi

--- loader.py
+++ loader.py
@@ -4,7 +4,6 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.client.session.aiohttp import AiohttpSession
 from data.config import settings
 from database.crud import Database
-from fastapi import FastAPI
 
 from utils.dropbox import DropboxClient
 from utils.sheet_utils.sheet_control import GoogleSheetsClient
@@ -14,7 +13,6 @@ bot = Bot(token=settings.BOT_TOKEN, parse_mode=ParseMode.HTML, session=session)
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
 dataBase = Database(settings.DB_PATH)
-appFA = FastAPI()
 client = GoogleSheetsClient(
     creds_path="quality-control-469712-5f601fa34788.json",
     sheet_id=settings.SHEET_ID
